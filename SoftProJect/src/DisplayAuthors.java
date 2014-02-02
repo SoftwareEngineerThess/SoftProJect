ed27752dff67d38f1acced0281e25b8f668d5bfc
petros313 <petros313@hotmail.com>
2014-02-03 00:29:38 +0200

    Θέλω να εκτελέσω ένα απλό ερώτημα στη βάση δεδομένων books, το οποίο ν' ανακτάει τον πίνακα Authors και να εμφανίζει τα δεδομένα που εμπεριέχει. Έχω εισάγει μερικές διασυνδέσεις της κλάσης και σε πρώτη φάση πρέπει να ορίσω τον ιστοσύνδεσμο της βάσης δεδομένων. Έχω βάλει τα πρώτα ορίσματα που χειρίζονται τη σύνδεση με τη βάση δεδομένων, αρχικοποιώ τη μεταβλητή για τις προτάσεις ερωτημάτων κι αρχικοποιώ και τη μεταβλητή που χειρίζεται τα παραγόμενα αποτελέσματα.

---
+++ SoftProJect/src/DisplayAuthors.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+//TO ONOMA THS BASHS KI O ISTOSYNDESMOS THS STON TOPIKO DIAKOMISTH   
+public class DisplayAuthors 
+{                        
+   static final String JDBC_DRIVER = "";        
+   static final String DATABASE_URL = "";
+   
+   //ENARXH THS SYNDESHS ME TH BASH BOOKS
+   public static void main( String args[] )
+   {
+      Connection connection = null; //SYNDESH ME TH BASH
+      Statement statement = null; //DHLWSEIS ERWTHMATWN
+   }
+}
