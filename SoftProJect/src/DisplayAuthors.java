317fae0ded7cfb8938ec046be6fcbd9a54febde8
stathisnikolaidis <stathis.nikolaidis@gmail.com>
2014-02-03 15:15:38 +0200

    Ολοκλήρωση της DisplayAuthors, της κλάσης δηλαδή που συνδέεται στη βάση books και κάνει το ακόλουθω ερώτημα:
    SELECT authorID, firstName, lastName FROM authors

--- SoftProJect/src/DisplayAuthors.java
+++ SoftProJect/src/DisplayAuthors.java
@@ -1,17 +1,76 @@
 import java.sql.Connection;
-import java.sql.Statement;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-//TO ONOMA THS BASHS KI O ISTOSYNDESMOS THS STON TOPIKO DIAKOMISTH   
+ 
 public class DisplayAuthors 
 {                        
    static final String JDBC_DRIVER = "";        
    static final String DATABASE_URL = "";
    
-   //ENARXH THS SYNDESHS ME TH BASH BOOKS
+   
    public static void main( String args[] )
    {
-      Connection connection = null; //SYNDESH ME TH BASH
-      Statement statement = null; //DHLWSEIS ERWTHMATWN
+      Connection connection = null;
+      Statement statement = null; 
+      try 
+      {
+         Class.forName( JDBC_DRIVER ); 
+
+         
+         connection = 
+            DriverManager.getConnection( DATABASE_URL, "jhtp6", "jhtp6" );
+
+         
+         statement = connection.createStatement();
+         
+        
+         ResultSet resultSet = statement.executeQuery( 
+            "SELECT authorID, firstName, lastName FROM authors" );
+         
+         
+         ResultSetMetaData metaData = resultSet.getMetaData();
+         int numberOfColumns = metaData.getColumnCount();
+         System.out.println( "Authors Table of Books Database:" );
+
+         for ( int i = 1; i <= numberOfColumns; i++ )
+            System.out.printf( "%-8s\t", metaData.getColumnName( i ) );
+         System.out.println();
+         
+         while ( resultSet.next() ) 
+         {
+            for ( int i = 1; i <= numberOfColumns; i++ )
+               System.out.printf( "%-8s\t", resultSet.getObject( i ) );
+            System.out.println();
+         } 
+      }  
+      catch ( SQLException sqlException ) 
+      {
+         sqlException.printStackTrace();
+         System.exit( 1 );
+      }
+      catch ( ClassNotFoundException classNotFound ) 
+      {
+         classNotFound.printStackTrace();            
+         System.exit( 1 );
+      } 
+      finally 
+      {                                                             
+         try                                                        
+         {                                                          
+            statement.close();                                      
+            connection.close();                                     
+         }                                              
+         catch ( Exception exception )                        
+         {                                                          
+            exception.printStackTrace();                                     
+            System.exit( 1 );                                       
+         }                                             
+      }    
+      
+      
    }
 }
